bf604df56cee526574c4492d96449d73494922c2
ephraim <ephraims.818@gmail.com>
2025-11-01 17:34:13 +0600

    JS--COMPLETE-JS---DATA-STRUCTURE - 02. Destructuring Objects

---
+++ 04. Data Structures/02. Destructuring Objects/script.js
@@ -0,0 +1,93 @@
+'use strict';
+
+// Data needed for first part of the section
+const restaurant = {
+  name: 'Classico Italiano',
+  location: 'Via Angelo Tavanti 23, Firenze, Italy',
+  categories: ['Italian', 'Pizzeria', 'Vegetarian', 'Organic'],
+  starterMenu: ['Focaccia', 'Bruschetta', 'Garlic Bread', 'Caprese Salad'],
+  mainMenu: ['Pizza', 'Pasta', 'Risotto'],
+
+  order: function (starterIndex, mainIndex) {
+    return [this.starterMenu[starterIndex], this.mainMenu[mainIndex]];
+  },
+
+  openingHours: {
+    thu: {
+      open: 12,
+      close: 22,
+    },
+    fri: {
+      open: 11,
+      close: 23,
+    },
+    sat: {
+      open: 0, // Open 24 hours
+      close: 24,
+    },
+  },
+
+  orderDelivery: function ({
+    starterIndex = 1,
+    mainIndex = 0,
+    time = '20:00',
+    address,
+  }) {
+    console.log(
+      `Order received! ${this.starterMenu[starterIndex]} and ${this.mainMenu[mainIndex]}. Your order will be delivered to ${address} at ${time}`
+    );
+  },
+};
+
+// Destructuring Objects
+const { name, openingHours, categories } = restaurant;
+console.log(name);
+console.log(categories);
+console.log(openingHours);
+
+// Changing variable names
+const {
+  name: restaurantName,
+  openingHours: hours,
+  categories: tags,
+} = restaurant;
+
+console.log(restaurantName);
+console.log(tags);
+console.log(hours);
+
+// Default values
+const { menu = [], starterMenu: starters = [] } = restaurant;
+console.log(menu);
+console.log(starters);
+
+// Mutating value
+let a = 111;
+let b = 999;
+console.log(a);
+console.log(b);
+
+const obj = { a: 23, b: 7, c: 14 };
+
+({ a, b } = obj);
+console.log(a);
+console.log(b);
+
+// Nested objects
+const {
+  fri: { open: o, close: c },
+} = openingHours;
+console.log(o);
+console.log(c);
+
+// Destructuring method arguments
+restaurant.orderDelivery({
+  time: '22:30',
+  address: 'Via del sole, 21',
+  mainIndex: 2,
+  starterIndex: 2,
+});
+restaurant.orderDelivery({
+  address: 'Via del sole, 21',
+  starterIndex: 1,
+});
