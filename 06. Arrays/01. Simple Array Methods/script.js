21be3efff3274360a9c180c3601eb9125d2af96f
ephraim <ephraims.818@gmail.com>
2025-11-12 11:44:25 +0600

    JS--COMPLETE-JS---ARRAYS - 01. Simple Array Methods

---
+++ 06. Arrays/01. Simple Array Methods/script.js
@@ -0,0 +1,37 @@
+'use strict';
+
+const currencies = new Map([
+  ['USD', 'United States dollar'],
+  ['EUR', 'Euro'],
+  ['GBP', 'Pound sterling'],
+]);
+
+const movements = [200, 450, -400, 3000, -650, -130, 70, 1300];
+
+let arr = ['a', 'b', 'c', 'd', 'e'];
+
+// Slice method
+console.log(arr.slice(2));
+console.log(arr.slice(2, 4));
+console.log(arr.slice(-2));
+console.log(arr.slice(-1));
+console.log(arr.slice(1, -2));
+console.log(arr.slice());
+
+// Splice
+// console.log(arr.splice(2));
+// console.log(arr.splice(-2));
+// console.log(arr.splice(1, 2));
+// console.log(arr);
+
+// Reverse
+const arr2 = ['j', 'i', 'h', 'g', 'f'];
+console.log(arr2.reverse());
+console.log(arr2);
+
+// Concat
+const letters = arr.concat(arr2);
+console.log(letters);
+
+// Join
+console.log(letters.join(' - '));
