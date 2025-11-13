4e5a5ababb491f3212cee15bb50d6c477a8850fb
ephraim <ephraims.818@gmail.com>
2025-11-13 20:25:17 +0600

    JS--COMPLETE-JS---ARRAYS - #Coding Challenge 03

---
+++ 06. Arrays/#Challenge - 03/script.js
@@ -0,0 +1,28 @@
+'use strict';
+// Coding Challenge #3
+
+/* 
+Rewrite the 'calcAverageHumanAge' function from the previous challenge, but this time as an arrow function, and using chaining!
+
+TEST DATA 1: [5, 2, 4, 1, 15, 8, 3]
+TEST DATA 2: [16, 6, 10, 5, 6, 1, 4]
+
+GOOD LUCK 😀
+*/
+
+/* ******************************************** */
+/*                   SOLUTION                   */
+/* ******************************************** */
+
+const test1 = [5, 2, 4, 1, 15, 8, 3];
+const test2 = [16, 6, 10, 5, 6, 1, 4];
+
+const calcAverageHumanAge = ages => {
+  return ages
+    .map(age => (age <= 2 ? 2 * age : 16 + age * 4))
+    .filter(age => age >= 18)
+    .reduce((acc, curr, index,arr) => acc + curr / arr.length, 0);
+};
+
+console.log(calcAverageHumanAge(test1));
+console.log(calcAverageHumanAge(test2));
