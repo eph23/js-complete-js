14afe5a87b5493ffc3042d80caa9840df7221461
ephraim <ephraims.818@gmail.com>
2025-11-15 19:05:37 +0600

    JS--COMPLETE-JS---ARRAYS - 15. More Ways of Creating and Filling Arrays

---
+++ 06. Arrays/15. Nore Ways of Creating and Filling Arrays/script.js
@@ -0,0 +1,21 @@
+'use strict';
+
+const arr = [1, 2, 3, 4, 5, 6, 7];
+console.log(arr);
+
+console.log(new Array(1, 2, 3, 4, 5, 6, 7));
+
+const x = new Array(7);
+
+// x.fill(1);
+x.fill(1, 3, 5);
+console.log(x);
+
+arr.fill(23, 2, 6);
+console.log(arr);
+
+const y = Array.from({ length: 7 }, () => 1);
+console.log(y);
+
+const z = Array.from({ length: 7 }, (_, i) => i + 1);
+console.log(z);
