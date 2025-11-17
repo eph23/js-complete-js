b1f3a6b81bee75db07df1b024cb0b2592ceefb31
ephraim <ephraims.818@gmail.com>
2025-11-17 14:21:18 +0600

    JS--COMPLETE-JS---NUMBERS-DATES - 01. Converting and Checking Numbers

---
+++ 07. Numbers Dates/01. Converting and Checking Numbers/script.js
@@ -0,0 +1,28 @@
+'use strict';
+
+console.log(23 === 23.0);
+console.log(Number('23'));
+console.log(+'23');
+
+// Parsing
+console.log(Number.parseInt('30px', 10));
+console.log(Number.parseInt('e23', 10));
+
+console.log(Number.parseInt('   2.5rem   '));
+console.log(Number.parseFloat('   2.5rem   '));
+
+// NaN
+console.log(Number.isNaN(20));
+console.log(Number.isNaN('20'));
+console.log(Number.isNaN(+'20x'));
+console.log(Number.isNaN(23 / 0));
+
+// Number
+console.log(Number.isFinite(20));
+console.log(Number.isFinite('20'));
+console.log(Number.isFinite(+'20X'));
+console.log(Number.isFinite(23 / 0));
+
+console.log(Number.isInteger(23));
+console.log(Number.isInteger(23.0));
+console.log(Number.isInteger(23 / 0));
