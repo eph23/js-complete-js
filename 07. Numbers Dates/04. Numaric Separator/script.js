477949303e77e40ab4b57e00ff26442b3aaa5ea9
ephraim <ephraims.818@gmail.com>
2025-11-17 17:31:53 +0600

    JS--COMPLETE-JS---NUMBERS-DATES - 04. Numaric Separator

---
+++ 07. Numbers Dates/04. Numaric Separator/script.js
@@ -0,0 +1,18 @@
+'use strict';
+
+const diameter = 287_460_000_000;
+console.log(diameter);
+
+const price = 345_99;
+console.log(price);
+
+const transferFee1 = 15_00;
+const transferFee2 = 1_500;
+console.log(transferFee1);
+console.log(transferFee2);
+
+const PI = 3.1415;
+console.log(PI);
+
+console.log(Number('230_000'));
+console.log(parseInt('230_000'));
