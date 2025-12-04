a03fa168b108552618e6828da4217dbc27b3da35
ephraim <ephraims.818@gmail.com>
2025-12-04 13:15:17 +0600

    JS--COMPLETE-JS---OOP - 07. Object.create

--- 09. OOP/06. Static Methods/script.js
+++ 09. OOP/06. Static Methods/script.js
@@ -1,24 +1,4 @@
 'use strict';
-const account = {
-  owner: 'Ephraim',
-  movements: [200, 530, 120, 300],
-
-  get latest() {
-    return this.movements.slice(-1).pop();
-  },
-
-  set latest(movement) {
-    this.movements.push(movement);
-  },
-};
-console.log(account.latest);
-
-account.latest = 50;
-console.log(account.latest);
-console.log(account.movements);
-
-('use strict');
-
 class PersonCl {
   constructor(fullName, birthYear) {
     this.fullName = fullName;
