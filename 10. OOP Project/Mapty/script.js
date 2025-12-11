3a21e6d5762f5fb9242c2ae145f361f6a4abb39f
ephraim <ephraims.818@gmail.com>
2025-12-12 05:43:06 +0600

    JS--COMPLETE-JS---OOP--MAPTY - 10. Working with localStorage

--- 10. OOP Project/Mapty/script.js
+++ 10. OOP Project/Mapty/script.js
@@ -84,6 +84,7 @@ class App {
 
   constructor() {
     this._getPosition();
+    this._getLocalStorage();
     form.addEventListener('submit', this._newWorkout.bind(this));
 
     inputType.addEventListener('change', this._toggleElevationField);
@@ -113,6 +114,10 @@ class App {
     }).addTo(this.#map);
 
     this.#map.on('click', this._showForm.bind(this));
+
+    this.#workouts.forEach(work => {
+      this._renderWorkoutMarker(work);
+    });
   }
   _showForm(mapEv) {
     this.#mapEvent = mapEv;
@@ -177,6 +182,7 @@ class App {
     this._renderWorkout(workout);
 
     this._hideForm();
+    this._setLocalStorage();
   }
 
   _renderWorkoutMarker(workout) {
@@ -266,6 +272,25 @@ class App {
         duration: 1,
       },
     });
+  }
+  _setLocalStorage() {
+    localStorage.setItem('workouts', JSON.stringify(this.#workouts));
+  }
+
+  _getLocalStorage() {
+    const data = JSON.parse(localStorage.getItem('workouts'));
+    console.log(data);
+
+    if (!data) return;
+    this.#workouts = data;
+    this.#workouts.forEach(work => {
+      this._renderWorkout(work);
+    });
+  }
+
+  reset() {
+    localStorage.removeItem('workouts');
+    location.reload();
   }
 }
 
